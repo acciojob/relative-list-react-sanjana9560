25227515d34f25d4f3c2a21b93f2485811f3c3fe
sanjana9560 <121418058+sanjana9560@users.noreply.github.com>
2025-10-24 01:05:29 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
 const App = () => {
+   const relatives = ["Rohan", "Priya", "Amit", "Neha"];
   return (
     <div id="main">
                {/* Do not remove the main div */}
+    {
+            relatives.map((item,index)=>{
+              return (
+                <>
+                <li keyq={`relativeListItem ${index+1}`}>{item}</li>
+                </>
+              )
+            })
+          }
     </div>
   )
 }
